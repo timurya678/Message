b32d08c51618e6404effcb1276c71372f1d1730c
timurya678 <timurya678@yandex.ru>
2018-06-23 20:36:15 +0500

    скобки

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Stack;
 import java.util.regex.*;
 
 interface IMessage {
@@ -6,10 +8,21 @@ interface IMessage {
     public int countWords();        // возвращает количество слов
     public String reverse();
     public int count(char c);
-    public int countRegex(char c);
+    public boolean isValid();
 }
 
 class Message implements IMessage {
+    private HashMap<Character, Character> couples = new HashMap<>();
+
+    {
+        couples.put('<','>');
+        couples.put('{','}');
+        couples.put('[',']');
+        couples.put('(',')');
+    }
+
+    private Stack<Character> stack = new Stack<>();
+
     private String message;
 
     public Message(String message) {
@@ -17,15 +30,17 @@ class Message implements IMessage {
     }
 
     // реализовать все методы интерфейса IMessage...
-
+    @Override
     public int countWords() {
         return message.trim().split("\\s+").length;
     }
 
+    @Override
     public String reverse() {
         return new StringBuilder(message).reverse().toString();
     }
 
+    @Override
     public int count(char c) {
         int count = 0;
         for (int i = 0; i < message.length(); i++) {
@@ -35,15 +50,23 @@ class Message implements IMessage {
         return count;
     }
 
-    public int countRegex (char c) {
-        Pattern pattern = Pattern.compile(Character.toString(c));
-        Matcher matcher = pattern.matcher(message);
-        int count = 0;
-        while (matcher.find()) {
-            count++;
+    @Override
+    public boolean isValid() {
+        stack.clear();
+        for (int i = 0; i < message.length(); i++) {
+            char character = message.charAt(i);
+            if (couples.containsKey(character))
+                stack.push(character);
+            if (couples.containsValue(character)) {
+                if (stack.empty()||couples.get(stack.pop())!=character) {
+                    return false;
+                }
+            }
         }
-        return count;
+            return (stack.empty());
+
     }
+
 
 
 }
@@ -51,10 +74,10 @@ class Message implements IMessage {
 
 public class Main {
     public static void main(String[] args) {
-        Message msg = new Message("s  Test, 123  message?  s");
+        Message msg = new Message("{s<  Te[st, 123  m>es]sage}?  s)");
         System.out.println(msg.countWords());
         System.out.println(msg.reverse());
         System.out.println(msg.count('e'));
-        System.out.println(msg.countRegex('s'));
+        System.out.println(msg.isValid());
     }
 }
